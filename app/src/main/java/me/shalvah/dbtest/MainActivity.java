73f3a73ab121b2654692b588da8cd4d63eb18aea
Shalvah <diakon.ng@gmail.com>
2016-10-12 19:40:07 +0100

    Update test app: set up UI for MainActivity

--- app/src/main/java/me/shalvah/dbtest/MainActivity.java
+++ app/src/main/java/me/shalvah/dbtest/MainActivity.java
@@ -3,12 +3,8 @@ package me.shalvah.dbtest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity
@@ -22,41 +18,6 @@ public class MainActivity extends AppCompatActivity
 			Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 			setSupportActionBar(toolbar);
 
-			FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-			fab.setOnClickListener(new View.OnClickListener()
-				{
-					@Override
-					public void onClick(View view)
-					{
-						Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-								.setAction("Action", null).show();
-					}
-				});
-		}
-
-		@Override
-		public boolean onCreateOptionsMenu(Menu menu)
-		{
-			// Inflate the menu; this adds items to the action bar if it is present.
-			getMenuInflater().inflate(R.menu.menu_main, menu);
-			return true;
-		}
-
-		@Override
-		public boolean onOptionsItemSelected(MenuItem item)
-		{
-			// Handle action bar item clicks here. The action bar will
-			// automatically handle clicks on the Home/Up button, so long
-			// as you specify a parent activity in AndroidManifest.xml.
-			int id = item.getItemId();
-
-			//noinspection SimplifiableIfStatement
-			if (id == R.id.action_settings)
-			{
-				return true;
-			}
-
-			return super.onOptionsItemSelected(item);
 		}
 
 		public void launch(View view)
@@ -66,7 +27,7 @@ public class MainActivity extends AppCompatActivity
 			switch (view.getId())
 			{
 				case R.id.studentsBtn:
-					intent = new Intent(context, CourseListActivity.class);
+					intent = new Intent(context, StudentListActivity.class);
 					break;
 				default:
 					intent = new Intent(context, CourseListActivity.class);
---
+++ app/src/main/java/me/shalvah/dbtest/StudentListActivity.java
@@ -0,0 +1,8 @@
+package me.shalvah.dbtest;
+
+/**
+ * Created by J on 12/10/2016.
+ */
+public class StudentListActivity
+	{
+	}
